06c86b4e0573d38344799bee0d4394346d43978e
AndreaRispoli97 <Andrea9715@outlook.it>
2025-09-09 18:35:52 +0200

    Add ManyToMany relationship between User and Role entities

--- src/main/java/org/lessons/java/spring/spring_la_mia_pizzeria_crud/model/User.java
+++ src/main/java/org/lessons/java/spring/spring_la_mia_pizzeria_crud/model/User.java
@@ -1,9 +1,15 @@
 package org.lessons.java.spring.spring_la_mia_pizzeria_crud.model;
 
+import java.util.Set;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -22,6 +28,18 @@ public class User {
     @NotBlank(message = "Username cannot be blank, empty or null")
     @Size(min = 6, message = "The password must have at least 6 characters")
     private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "role_user", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+
+    public Set<Role> getRoles() {
+        return this.roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 
     public Integer getId() {
         return this.id;
